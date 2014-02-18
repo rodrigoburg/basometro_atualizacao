c90ddb443ff515b8c7b53b19ea6a2d4ce7f36933
Estadao Dados <estadaodados@gmail.com>
2014-02-18 16:55:18 -0300

    Adequando os arquivos aos padrões de estilo do python

--- atualiza_deputados.py
+++ atualiza_deputados.py
@@ -5,65 +5,77 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import csv
 
-#retorna uma lista com os códigos de todos os deputados que estão no arquivo local
+
 def busca_deputados_registrados():
-    with open("deputados.csv","r") as file:
+    """Retorna uma lista com os códigos de todos os deputados
+        que estão no arquivo local"""
+    with open("deputados.csv", "r") as file:
         arquivo = csv.reader(file)
-        arquivo.next() #ignora o cabeçalho
-        dep_antigos = [row[0] for row in arquivo] # a primeira coluna é a do ID
-        print("Há "+str(len(dep_antigos))+" deputados registrados no arquivo salvo.")
+        arquivo.next()  # ignora o cabeçalho
+        # a primeira coluna é a do ID
+        dep_antigos = [row[0] for row in arquivo]
+        print("Há " + str(len(dep_antigos)) + " deputados registrados \
+              no arquivo salvo.")
         return dep_antigos
 
-#consulta a API da Câmara para os deputados e retorna o XML só com os campos de deputado
+
 def consulta_API_deputados():
+    """Consulta a API da Câmara para os deputados e retorna o XML \
+        só com os campos de deputado"""
     url = "http://www.camara.gov.br/sitcamaraws/deputados.asmx/ObterDeputados"
     connection = urlopen(url)
     data = connection.read()
     bs = BeautifulSoup(data)
     return bs.findAll("deputado")
 
-#de acordo com a consulta na API, grava os novos deputados que não estiverem já listados no csv antigo
+
 def salva_deputados(deputados):
+    """De acordo com a consulta na API, grava os novos deputados que não \
+        estiverem já listados no csv antigo"""
     #prepara o arquivo de saída
     contador = 0
     with open("deputados.csv", "a+") as arquivo:
         reader = csv.reader(arquivo)
-        writer = csv.writer(arquivo, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer = csv.writer(arquivo,
+                            delimiter=',',
+                            quotechar='"',
+                            quoting=csv.QUOTE_ALL)
 
         #Verificando de o arquivo já existe.
-            # Se não existe, ele será automaticamente criado, daí basta adicionarmos
-            # o "header" da primeira linha.
-        arquivo.seek(0) #garante que a leitura está no começo do arquivo
+            # Se não existe, ele será automaticamente criado, daí basta
+            # adicionarmos o "header" da primeira linha.
+        arquivo.seek(0)  # garante que a leitura está no começo do arquivo
         primeiro_caractere = arquivo.readline()
         if not primeiro_caractere:
-            print("Não há arquivo de deputados no diretório local. Criando arquivo em branco...")
-            # Escreve a primeira linha com o cabeçalho caso o arquivo esteja vazio.
-            writer.writerow(["id","nome","partido"])
+            # Escreve a primeira linha com o cabeçalho caso o
+                # arquivo esteja vazio.
+            writer.writerow(["id", "nome", "partido"])
         else:
             print("Arquivo de deputados no diretório local foi encontrado.")
-            arquivo.seek(0) # volta o leitor para o início do arquivo
+            arquivo.seek(0)  # volta o leitor para o início do arquivo
 
         # gera uma lista com todos os ids constantes do arquivo
         lista_ids_salvos = [row[0] for row in reader]
-        print("Há "+str(len(lista_ids_salvos))+" deputados registrados no arquivo salvo.")
+        print("Há " + str(len(lista_ids_salvos)) + "\
+              deputados registrados no arquivo salvo.")
 
         # iterando na lista de deputados enviada pela API
         for d in deputados:
-            # se o id não estiver na lista atual, adicione uma nova linha com os seus dados
+            # se o id não estiver na lista atual, adicione uma nova
+                # linha com os seus dados
             if d.idparlamentar.string not in lista_ids_salvos:
-                writer.writerow([d.idparlamentar.string,d.nome.string,d.partido.string])
+                writer.writerow([d.idparlamentar.string,
+                                 d.nome.string,
+                                 d.partido.string])
                 contador += 1
 
-    print("Foram adicionados "+str(contador)+" deputados no arquivo local.")
+    print("Foram adicionados " + str(contador) + "\
+          deputados no arquivo local.")
 
-#função que articula todas as anteriores e faz todo o processo de atualização
+
 def atualiza_deputados():
-    #dep_antigos = []
-    #arquivo = existe_arquivo_deputados()
-    #if (arquivo):
-    #    dep_antigos = busca_deputados_antigos()
-    #else:
-    #    cria_arquivo_vazio()
+    """Função que articula todas as anteriores e faz todo o
+        processo de atualização"""
     deputados = consulta_API_deputados()
     salva_deputados(deputados)
 
--- atualiza_proposicoes.py
+++ atualiza_proposicoes.py
@@ -2,83 +2,140 @@
 #!/usr/bin/python3
 
 # SOBRE ESTE ARQUIVO
-# Este script está dividido em várias pequenas funções, que são depois coordenadas pela função principal obter_proposicoes(ano). O objetivo final dessa função é consultar a API da Câmara para criar dois arquivos no diretório local: o proposicoes.csv, com a lista de todas as proposições votadas no ano em questão com suas informações principais (data da votação, ementa, orientação do governo, etc), e o votos.csv, com a lista de como cada deputado votou em cada uma dessas votações. 
+# Este script está dividido em várias pequenas funções, que são
+# depois coordenadas pela função principal obter_proposicoes(ano).
+# O objetivo final dessa função é consultar a API da Câmara para
+# criar dois arquivos no diretório local: o proposicoes.csv, com a
+# lista de todas as proposições votadas no ano em questão com suas
+# iformações principais (data da votação, ementa, orientação do
+# governo, etc), e o votos.csv, com a lista de como cada deputado
+# votou em cada uma dessas votações.
 
-# Se os arquivos já existirem, a função checa se há registros no proposicoes.csv para o ano em que se está atualizando. Se houver, ela irá acrescentar apenas os registros que estão no site da Câmara mas não no arquivo local de proposições. Se não houver registros para esse ano, a função irá simplesmente adicionar todos que retornarem da API da Câmara como votados em plenário no ano em questão. Os votos de cada deputado serão acrescentados no arquivo votos.csv para toda votação que for acrescentada seguindo os critérios descritos acima.
+# Se os arquivos já existirem, a função checa se há registros no
+# proposicoes.csv para o ano em que se está atualizando. Se houver,
+# ela irá acrescentar apenas os registros que estão no site da Câmara
+# mas não no arquivo local de proposições. Se não houver registros
+# para esse ano, a função irá simplesmente adicionar todos que
+# retornarem da API da Câmara como votados em plenário no ano ementa
+# questão. Os votos de cada deputado serão acrescentados no arquivo
+# votos.csv para toda votação que for acrescentada seguindo os
+# critérios descritos acima.
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import csv
 
-#checa se há arquivo de proposicoes no diretório local. se houver, ele já retorna esse arquivo
+
 def existe_arquivo_proposicoes():
-    try: 
-        with open("proposicoes.csv","r") as file: 
-            arquivo = csv.reader(file)
+    """ Checa se há arquivo de proposicoes no diretório local. se houver,
+        ele já retorna esse arquivo"""
+    try:
+        with open("proposicoes.csv", "r") as file:
             print("Arquivo de votações no diretório local foi encontrado.")
-            return True
-    except FileNotFoundError:
-        print("Não há arquivo de votações no diretório local. Criando arquivo em branco...")
+            return file
+    except IOError:
+        print("Não há arquivo de votações no diretório local.")
         return False
 
-#cria um arquivo vazio de proposicoes caso não exista no diretório local
+
 def cria_arquivo_vazio_proposicoes():
+    """ Cria um arquivo vazio de proposicoes caso não exista
+        no diretório local"""
     with open("proposicoes.csv", "w", encoding='UTF8') as file:
-        writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-        writer.writerow(["codigo","tipo","numero","ano","data_votacao","hora_votacao","ementa","resumo","orientacao_governo","num_votacoes"])
+        writer = csv.writer(
+            file,
+            delimiter=',',
+            quotechar='"',
+            quoting=csv.QUOTE_ALL)
+        writer.writerow(
+            ["codigo",
+             "tipo",
+             "numero",
+             "ano",
+             "data_votacao",
+             "hora_votacao",
+             "ementa",
+             "resumo",
+             "orientacao_governo",
+             "num_votacoes"])
 
-#checa se há arquivo de votos no diretório local 
+
 def existe_arquivo_votos():
-    try: 
-        with open("votos.csv","r") as arquivo:
+    """ Checa se há arquivo de votos no diretório local"""
+    try:
+        with open("votos.csv", "r") as arquivo:
             print("Arquivo de votos no diretório local foi encontrado.")
-            return True
-    except FileNotFoundError:
-        print("Não há arquivo de votos no diretório local. Criando arquivo em branco...")
+            return arquivo
+    except IOError:
+        print("Não há arquivo de votos no diretório local.")
         return False
 
-#cria um arquivo vazio de votos caso não exista no diretório local
-def cria_arquivo_vazio_votos():
-    with open("votos.csv", "w", encoding='UTF8') as file:
-        writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-        writer.writerow(["codigo_votacao","id_deputado","nome_deputado","partido","voto"])
 
-#retorna uma lista com os códigos de todas as proposições que estão no arquivo local, no ano pesquisado
+def cria_arquivo_vazio_votos():
+    """ Cria um arquivo vazio de votos caso não exista no diretório local"""
+    with open("votos.csv", "w", encoding='UTF8') as file:
+        writer = csv.writer(
+            file,
+            delimiter=',',
+            quotechar='"',
+            quoting=csv.QUOTE_ALL)
+        writer.writerow(["codigo_votacao",
+                         "id_deputado",
+                         "nome_deputado",
+                         "partido",
+                         "voto"])
+
+
 def busca_proposicoes_antigas(ano):
+    """ Retorna uma lista com os códigos de todas as proposições que
+        estão no arquivo local, no ano pesquisado"""
+
     prop_antigas = []
-    with open("proposicoes.csv","r") as file:
-        arquivo = csv.reader(file) 
-        next(arquivo, None) #ignora o cabeçalho
+    with open("proposicoes.csv", "r") as file:
+        arquivo = csv.reader(file)
+        arquivo.next()  # ignora o cabeçalho
         for row in arquivo:
-            if row[4][-4:] == ano: #só adiciona na lista as do mesmo ano que está sendo atualizado
-                prop_antigas.append(row[0]) #a primeira coluna é a do codigo
-        print("Há "+str(len(prop_antigas))+" votações de "+str(ano)+" no arquivo salvo.")
+            # só adiciona na lista as do mesmo ano que está sendo atualizado
+            if row[4][-4:] == ano:
+                # a primeira coluna é a do codigo
+                prop_antigas.append(row[0])
+        print("Há " + str(len(prop_antigas)) +
+              " votações de " + str(ano) +
+              " no arquivo salvo.")
         return prop_antigas
 
-#função que busca o API da Câmara e retorna o XML de todas as votações de um determinado ano
+
 def pega_todas_proposicoes(ano):
-    url = "http://www.camara.gov.br/SitCamaraWS/Proposicoes.asmx/ListarProposicoesVotadasEmPlenario?ano="+ano+"&tipo="
+    """ Função que busca o API da Câmara e retorna o XML
+        de todas as votações de um determinado ano"""
+    url = "http://www.camara.gov.br/SitCamaraWS/Proposicoes.asmx/\
+            ListarProposicoesVotadasEmPlenario?ano=" + ano + "&tipo="
     connection = urlopen(url)
     data = connection.read()
     bs = BeautifulSoup(data)
     return bs.findAll("proposicao")
 
-#função que pega os dados extras de cada proposição, por meio de duas consultas diferentes
+
 def obter_dados_proposicao(prop):
+    """Função que pega os dados extras de cada proposição,
+        por meio de duas consultas diferentes"""
     prop = pega_dados_API_proposicao(prop)
     prop = pega_dados_API_votacoes(prop)
     return prop
 
-#pega os dados da proposicao de acordo com a API de proposicoes
+
 def pega_dados_API_proposicao(prop):
-    url = "http://www.camara.gov.br/SitCamaraWS/Proposicoes.asmx/ObterProposicaoPorID?IdProp="+prop["codigo"]    
+    """Pega os dados da proposicao de acordo com a API de proposicoes"""
+    url = "http://www.camara.gov.br/SitCamaraWS/Proposicoes.asmx/\
+        ObterProposicaoPorID?IdProp=" + prop["codigo"]
     connection = urlopen(url)
     data = connection.read()
     bs = BeautifulSoup(data)
     prop["tipo"] = bs.proposicao["tipo"].strip()
     prop["numero"] = bs.proposicao["numero"]
     prop["ano"] = bs.proposicao["ano"]
-    ## pega apenas a nova ementa nas proposições em que ela tiver sido atualizada
+    ## pega apenas a nova ementa nas proposições
+        #em que ela tiver sido atualizada
     if "NOVA EMENTA:" in bs.ementa.string:
         ementa = bs.ementa.string.split("NOVA EMENTA:")
         prop["ementa"] = ementa[1].strip()
@@ -86,17 +143,20 @@ def pega_dados_API_proposicao(prop):
         prop["ementa"] = bs.ementa.string.strip()
     return prop
 
-#pega os dados da proposicao de acordo com a API de proposicoes
+
 def pega_dados_API_votacoes(prop):
-    url = "http://www.camara.gov.br/SitCamaraWS/Proposicoes.asmx/ObterVotacaoProposicao?tipo="+prop["tipo"]+"&numero="+prop["numero"]+"&ano="+prop["ano"]
-    try: 
+    """Pega os dados da proposicao de acordo com a API de proposicoes"""
+    url = "http://www.camara.gov.br/SitCamaraWS/Proposicoes.asmx/\
+        ObterVotacaoProposicao?tipo=" + prop["tipo"] + "&\
+        numero=" + prop["numero"] + "&ano=" + prop["ano"]
+    try:
         connection = urlopen(url)
         data = connection.read()
-    except: 
+    except:
         return prop
     bs = BeautifulSoup(data)
     votacoes = bs.findAll("votacao")
-    
+
     prop["num_votacoes"] = 0
     prop["data_votacao"] = []
     prop["hora_votacao"] = []
@@ -107,78 +167,117 @@ def pega_dados_API_votacoes(prop):
     votos = {}
 
     #agora ele pega todas as informações para cada votação ocorrida no ano
-    for v in votacoes: 
-        if v["data"][-4:] == prop["ano_votacao"]: #retira votações de outros anos
+    for v in votacoes:
+        #retira votações de outros anos
+        if v["data"][-4:] == prop["ano_votacao"]:
             prop["num_votacoes"] += 1
             prop["data_votacao"].append(v["data"])
             prop["hora_votacao"].append(v["hora"])
             prop["resumo"].append(v["resumo"].strip())
-            
-            try: #testa se há ou não há orientações para essa votação e pega esses dados
-                sigla = [o["sigla"].strip() for o in v.orientacaobancada.findAll("bancada")]
-                orientacao = [o["orientacao"].strip() for o in v.orientacaobancada.findAll("bancada")]
+
+            try:
+                #testa se há ou não há orientações para
+                #essa votação e pega esses dados
+                sigla = [o["sigla"].strip()
+                         for o in v.orientacaobancada.findAll("bancada")]
+                orientacao = [o["orientacao"].strip()
+                              for o in v.orientacaobancada.findAll("bancada")]
                 orientacoes = dict(zip(sigla, orientacao))
-                prop["orientacao_governo"].append(orientacoes.get("GOV.","Não existe"))
+                prop["orientacao_governo"].append(
+                    orientacoes.get("GOV.", "Não existe"))
                 prop["orientacoes"].append(orientacoes)
-            except: 
+            except:
                 prop["orientacao_governo"].append("Não existe")
-                        
-            try: #testa e pega dados das votações  
-                votos["idecadastro"] = [v["idecadastro"] for v in v.votos.findAll("deputado")]
-                votos["nome"] = [v["nome"] for v in v.votos.findAll("deputado")]
-                votos["voto"]= [v["voto"].strip() for v in v.votos.findAll("deputado")]
-                votos["partido"] = [v["partido"].strip() for v in v.votos.findAll("deputado")]
+
+            try:
+                #testa e pega dados das votações
+                votos["idecadastro"] = [v["idecadastro"]
+                                        for v in v.votos.findAll("deputado")]
+                votos["nome"] = [v["nome"]
+                                 for v in v.votos.findAll("deputado")]
+                votos["voto"] = [v["voto"].strip()
+                                 for v in v.votos.findAll("deputado")]
+                votos["partido"] = [v["partido"].strip()
+                                    for v in v.votos.findAll("deputado")]
                 prop["votos"].append(votos)
             except:
                 pass
     return prop
-        
-#de acordo com a consulta na API, grava as novas proposicoes que não estiverem já listados no csv antigo
-def adiciona_novas_proposicoes(proposicoes,prop_antigas,ano):
+
+
+def adiciona_novas_proposicoes(proposicoes, prop_antigas, ano):
+    """De acordo com a consulta na API, grava as novas proposicoes
+        que não estiverem já listados no csv antigo"""
     contador = 0
     prop = {}
     #prepara os dois arquivos de saída
-    with open("proposicoes.csv", "a", encoding='UTF8') as prop_saida, open("votos.csv", "a", encoding='UTF8') as voto_saida:
-        escreve_prop = csv.writer(prop_saida, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-        escreve_voto = csv.writer(voto_saida, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-        
+    with open("proposicoes.csv", "a", encoding='UTF8') as prop_saida,\
+            open("votos.csv", "a", encoding='UTF8') as voto_saida:
+        escreve_prop = csv.writer(
+            prop_saida,
+            delimiter=',',
+            quotechar='"',
+            quoting=csv.QUOTE_ALL)
+        escreve_voto = csv.writer(voto_saida,
+                                  delimiter=',',
+                                  quotechar='"',
+                                  quoting=csv.QUOTE_ALL)
+
         #loop que escreve as votações e os votos
         for p in proposicoes:
-            
-            #se o id não estiver na lista atual, adicione uma nova linha com os seus dados
+
+            #se o id não estiver na lista atual,
+            #adicione uma nova linha com os seus dados
             if p.codproposicao.string not in prop_antigas:
                 prop["ano_votacao"] = ano
                 prop["codigo"] = p.codproposicao.string
                 prop = obter_dados_proposicao(prop)
-                
+
                 #loop para adicionar todas as votacoes no mesmo ano
-                for i in range(prop["num_votacoes"]): 
+                for i in range(prop["num_votacoes"]):
                     contador += 1
-                    escreve_prop.writerow([prop["codigo"],prop["tipo"],prop["numero"],prop["ano"],prop["data_votacao"][i-1],prop["hora_votacao"][i-1],prop["ementa"],prop["resumo"][i-1],prop["orientacao_governo"][i-1],  prop["num_votacoes"]])
-                    
-                    #loop para adicionar uma linha para cada deputado no arquivo de votos
-                    try: 
-                        for d in range(len(prop["votos"][i-1]["voto"])):
-                            escreve_voto.writerow([prop["codigo"],prop["votos"][i-1]["idecadastro"][d],prop["votos"][i-1]["nome"][d],prop["votos"][i-1]["partido"][d],prop["votos"][i-1]["voto"][d]])
+                    escreve_prop.writerow([prop["codigo"],
+                                           prop["tipo"],
+                                           prop["numero"],
+                                           prop["ano"],
+                                           prop["data_votacao"][i - 1],
+                                           prop["hora_votacao"][i - 1],
+                                           prop["ementa"],
+                                           prop["resumo"][i - 1],
+                                           prop["orientacao_governo"][i - 1],
+                                           prop["num_votacoes"]])
+
+                    #loop para adicionar uma linha para cada
+                    # deputado no arquivo de votos
+                    try:
+                        for d in range(len(prop["votos"][i - 1]["voto"])):
+                            escreve_voto.writerow(
+                                [prop["codigo"],
+                                 prop["votos"][i - 1]["idecadastro"][d],
+                                 prop["votos"][i - 1]["nome"][d],
+                                 prop["votos"][i - 1]["partido"][d],
+                                 prop["votos"][i - 1]["voto"][d]])
                     except:
-                        pass   
+                        pass
 
-    print("Foram adicionadas "+str(contador)+" votações no arquivo local.")
+    print("Foram adicionadas " + str(contador) + " votações no arquivo local.")
 
-#obtem todas as proposições votadas em um determinado ano articulando as funções anteriores
+
 def obter_proposicoes(ano):
+    """obtem todas as proposições votadas em um determinado ano
+        articulando as funções anteriores"""
     prop_antigas = []
-    
+
     if existe_arquivo_proposicoes():
         prop_antigas = busca_proposicoes_antigas(ano)
     else:
         cria_arquivo_vazio_proposicoes()
-    
+
     if not existe_arquivo_votos():
         cria_arquivo_vazio_votos()
-    
-    proposicoes = pega_todas_proposicoes(ano)
-    adiciona_novas_proposicoes(proposicoes,prop_antigas,ano)
 
-    
+    proposicoes = pega_todas_proposicoes(ano)
+    adiciona_novas_proposicoes(proposicoes, prop_antigas, ano)
+
+
 obter_proposicoes("2013")
