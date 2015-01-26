8a3200e64e4974c993a6e09c16ae919988e2fe82
Rodrigo Burgarelli <rodrigo.burgarelli1@gmail.com>
2015-01-26 15:20:32 -0200

    senado com fotos

--- atualizacao/senado/checa_senadores.py
+++ atualizacao/senado/checa_senadores.py
@@ -4,6 +4,7 @@
 from pandas import DataFrame, read_csv
 from unicodedata import normalize
 from urllib.request import urlopen
+import urllib.request
 from bs4 import BeautifulSoup
 import os
 import csv
@@ -59,7 +60,7 @@ def acha_senador(senadores):
                 senador['ID'] = sen.codigoparlamentar.string
                 senador['ANO_MANDATO'] = "--"
                 senador['LEGISLATURA'] = sen.legislaturainicio.string + "/" + sen.legislaturafim.string
-                senador['URL_FOTO'] = sen.urlfotoparlamentar.string
+                senador['URL_FOTO'] = ""
                 politicos = politicos.append(senador,ignore_index=True)
                 print("Adicionado político: "+nome)
     politicos.to_csv(path+"senadores.csv",sep=";",index=False, quoting=csv.QUOTE_ALL)
@@ -71,10 +72,45 @@ def descompactar_arquivos():
 def compactar_arquivos():
     os.system("bzip2 -9 "+path+"*.csv "+path+"*.json")
 
+def baixa_fotos():
+    #cria diretório paras as fotos, se não houver
+    if not os.path.isdir(path+"/"+mandato+"/fotos"):
+        print("Criando diretório para as fotos")
+        os.system("mkdir "+path+"/"+mandato)
+        os.system("mkdir "+path+"/"+mandato+"/fotos")
+    
+    politicos = read_csv(path+"senadores.csv",sep=";")
+    sen_sem_foto = politicos[politicos.URL_FOTO.isnull()]
+    sen_sem_foto = list(sen_sem_foto["NOME_CASA"])
+    
+    
+    #url = "file://"+path+"/Deputados.xml"
+    url = "http://legis.senado.leg.br/dadosabertos/senador/lista/legislatura/54"
+    bs = BeautifulSoup(urlopen(url).read())
+    senadores = bs.findAll("parlamentar")
+    for s in senadores:
+        nome = remover_acentos(s.nomeparlamentar.string)
+        if nome in sen_sem_foto:
+            codigo = str(list(politicos[politicos.NOME_CASA == nome]["ID"])[0])
+            try:
+                urllib.request.urlretrieve(s.urlfotoparlamentar.string, path+"/"+mandato+"/fotos/"+codigo+".jpg")
+                politicos.loc[politicos.NOME_CASA == nome,"URL_FOTO"] = codigo+".jpg"
+            except urllib.error.HTTPError:
+                politicos.loc[politicos.NOME_CASA == nome,"URL_FOTO"] = "sem_foto.jpg"
+                
+            
+            print(s.urlfotoparlamentar.string)
+            #politicos.loc[politicos.POLITICO == dep_sem_acento]["URL_FOTO"] = d.urlfoto.string
+#    politicos["ANO_MANDATO"] = politicos["ANO_MANDATO"].apply(int)
+#    politicos["LEGISLATURA"] = politicos["LEGISLATURA"].apply(int)
+    politicos.loc[politicos.URL_FOTO.isnull(),"URL_FOTO"] = "sem_foto.jpg"
+    politicos.to_csv(path+"/senadores.csv",sep=";",index=False, quoting=csv.QUOTE_ALL)
 
+mandato = "dilma1"
 path = os.path.dirname(os.path.abspath(__file__))+"/"
 
-descompactar_arquivos()
-limpar_votos()
-testa_voto()
+#descompactar_arquivos()
+#limpar_votos()
+#testa_voto()
+#baixa_fotos()
 compactar_arquivos()
